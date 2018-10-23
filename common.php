cfa6571988784deddc890d06ef1363b33f260ca4
PatricNox <eronoxsmail2@gmail.com>
2018-10-23 15:30:38 +0200

    Move routing initialiser to setup handler

--- common.php
+++ common.php
@@ -17,22 +17,11 @@ if (!defined('IN_NOXCMS'))
     exit;
 
 use NoxCMS\Client\Http\Request;
-use NoxCMS\Client\Http\Router;
-
 require($_root . 'includes/startup.' . $phpEx);
 require($_root . 'includes/functions.' . $phpEx);
 
-// Setup routes for the site.
-// Staff respective User (default) view
-$router = new Router([
-    // DEFAULT
-    ''           => $_root.'/NoxCMS/controllers/web.php',
-    // ADMIN CONTROL PANEL
-    'admin'      => $_root.'/NoxCMS/controllers/acp.php',
-    // INSTALLER
-    # TODO: Route specify /install along with folder 
-    'installer'    => $_root.'/NoxCMS/controllers/installer.php',
-]);
+// Initialise our base routes
+require $router->navigate(Request::uri());
 
  // Redirect the user to the installer
 if (!defined('NOXCMS_INSTALLED'))
@@ -41,7 +30,5 @@ if (!defined('NOXCMS_INSTALLED'))
     if (defined('IN_INSTALL') && IN_INSTALL == true)
         return;
 
-    $router->redirect('installer');
+    require_once $router->redirect('installer');
 }
-
-require $router->navigate(Request::uri());
--- includes/startup.php
+++ includes/startup.php
@@ -16,6 +16,8 @@
 if (!defined('IN_NOXCMS'))
     exit;   
 
+use NoxCMS\Client\Http\Router;
+
 // Define whether the CMS is installed or not
 if (!file_exists($_root . 'install/init.php'))
     define('NOXCMS_INSTALLED', true);
@@ -31,3 +33,15 @@ if (!file_exists($_root . 'vendor/autoload.php'))
 
 else
     require($_root . 'vendor/autoload.php');
+
+// Setup the main routes for the site.
+// User view, Admin view and the install page.
+$router = new Router([
+    // DEFAULT
+    ''           => $_root.'NoxCMS/controllers/web.php',
+    // ADMIN CONTROL PANEL
+    'admin'      => $_root.'NoxCMS/controllers/acp.php',
+    // INSTALLER
+    # TODO: Route specify /install along with folder 
+    'installer'  => $_root.'NoxCMS/controllers/installer.php',
+]);
