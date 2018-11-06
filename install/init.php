6e5364060ddc594a98695d8d2f22dd9618c4ef9f
PatricNox <eronoxsmail2@gmail.com>
2018-11-06 14:52:39 +0100

    Prevent direct access to install script page

--- install/init.php
+++ install/init.php
@@ -8,3 +8,10 @@
 * @license GNU General Public License, version 2 (GPL-2.0)
 *
 */
+
+if (strtoupper($_SERVER['REQUEST_METHOD']) !== 'POST')
+{
+  header("Location: javascript://history.go(-1)");
+  exit;
+}
+   
