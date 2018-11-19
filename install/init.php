291af3dfbaef4af22656a6ce7fe78e20ec368a9c
PatricNox <eronoxsmail2@gmail.com>
2018-11-19 23:54:43 +0100

    Add more startup post examples

--- install/init.php
+++ install/init.php
@@ -152,7 +152,10 @@ $install->query("
     USE noxcms;
     -- WELCOME POST
     INSERT INTO post_body(author_id, post_title, content, public)
-    VALUES(1, 'Welcome', 'Welcome to NoxCMS!\n\n to begin, visit /admin', 1);
+    VALUES
+    (1, 'Welcome', 'Welcome to NoxCMS!\n\n to begin, visit /admin', 1),
+    (1, 'About', 'Did you know that this CMS is made by github.com/PatricNox?', 1),
+    (1, 'Fun fact', 'There are no third party libraries used so far whatsoever! \n\neven though it surely would\'ve helped alot..', 1);
     ");
 
 $p = $_SERVER['DOCUMENT_ROOT'];
